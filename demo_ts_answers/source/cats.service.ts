a44119c937a13f51cedcef070baa1752a80e5843
Nicholas Johnson <nicholas@forwardadvance.com>
2016-05-25 09:45:38 +0100

    missing dependencies

---
+++ demo_ts_answers/source/cats.service.ts
@@ -0,0 +1,45 @@
+import {Injbectable} from '@angular/core';
+
+@Injectable()
+export class Cats {
+  get() {
+    return [
+      {
+        name: "Cheetara",
+        specialSkill: "speed",
+        planet: "Thundera",
+        image: "images/cheetara.jpg"
+      },
+      {
+        name: "Lion-O",
+        specialSkill: "Sword of Omens",
+        planet: "Thundera",
+        image: "images/lion-o.jpg"
+      },
+      {
+        name: "Panthro",
+        specialSkill: "Mechanical Engineering",
+        planet: "Thundera",
+        image: "images/panthro.jpg"
+      },
+      {
+        name: "Jaga the Wise",
+        specialSkill: "Wisdom",
+        planet: "Thundera",
+        image: "images/jaga.jpg"
+      },
+      {
+        name: "Snarf",
+        specialSkill: "Annoyance",
+        planet: "Thundera",
+        image: "images/snarf.jpg"
+      },
+      {
+        name: "Robear Berbil",
+        specialSkill: "Farming",
+        planet: "Ro-Bear",
+        image: "images/berbils.jpg"
+      }
+    ]
+  }
+}
